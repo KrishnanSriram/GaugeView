5d86f4d7113ec8231064b0b9cfd22d7d55275957
Krishnan Sriram Rama <krishnan.sriram@hotmail.com>
2017-02-06 07:20:00 -0500

    first commit

---
+++ SFGaugeView/SFGaugeView.swift
@@ -0,0 +1,21 @@
+//
+//  SFGaugeView.swift
+//  SFGaugeView
+//
+//  Created by Krishnan Sriram Rama on 2/5/17.
+//  Copyright © 2017 Krishnan Sriram Rama. All rights reserved.
+//
+
+import UIKit
+
+class SFGaugeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SFGaugeView/UIImage+Rotation.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage+Rotation.swift
+//  SFGaugeView
+//
+//  Created by Krishnan Sriram Rama on 2/5/17.
+//  Copyright © 2017 Krishnan Sriram Rama. All rights reserved.
+//
+
+import Foundation
